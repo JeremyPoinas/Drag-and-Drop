d6ccc575adbbd273674daf8ebf726959839b57e5
JeremyPoinas <poinas.jeremy@gmail.com>
2023-05-25 16:05:50 +0200

    Fetching user input

--- src/app.ts
+++ src/app.ts
@@ -40,10 +40,39 @@ class ProjectInput {
     this.attach();
   }
   
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again');
+      return; 
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs(): void {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(`title: ${title}, description: ${description}, people: ${people}`);
+      this.clearInputs();
+    }
+
   }
 
   private attach() {
